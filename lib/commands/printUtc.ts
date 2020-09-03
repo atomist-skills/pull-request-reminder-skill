efdcf0eb4d59125863994c468a5b549d1f66b14d
Christian Dupuis <cd@atomist.com>
2020-09-03 22:03:58 +0200

    Make the message a string

--- lib/commands/printUtc.ts
+++ lib/commands/printUtc.ts
@@ -1,4 +1,4 @@
 import { CommandHandler } from "@atomist/skill";
 
 export const handler: CommandHandler = async ctx =>
-	ctx.message.respond(new Date().getUTCHours());
+	ctx.message.respond(`${new Date().getUTCHours()} hours`);
