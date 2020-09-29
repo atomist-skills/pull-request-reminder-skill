dd7476b30a4e0cb5e4da5d1c73448ca116ba5436
Christian Dupuis <cd@atomist.com>
2020-09-29 18:35:31 +0200

    Unhide schedule parameter

--- skill.ts
+++ skill.ts
@@ -18,7 +18,6 @@ import {
 	Category,
 	parameter,
 	ParameterType,
-	ParameterVisibility,
 	resourceProvider,
 	skill,
 } from "@atomist/skill";
@@ -57,7 +56,7 @@ export const Skill = skill<RemindConfiguration & { schedule: any; repos: any }>(
 				defaultValue: "0 * * * *",
 				description: "Cron expression to process pull request reviews",
 				required: false,
-				visibility: ParameterVisibility.Hidden,
+				// visibility: ParameterVisibility.Hidden,
 			},
 			repos: parameter.repoFilter(),
 		},
